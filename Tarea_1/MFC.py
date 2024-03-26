60af8ef43e83e939450e73a84e91c98d17b4f58a
Javier Molina <javier.molina@mail.udp.cl>
2024-03-25 22:17:52 -0300

    Update MFC.py

--- Tarea_1/MFC.py
+++ Tarea_1/MFC.py
@@ -1,4 +1,15 @@
+from Tree import Tree
+from Problem import Problem
+
 class MFC:
-    def __init__(self) -> None:
+    tree: Tree = None
+    problem: Problem = None
+
+    def __init__(self, data_path, start_X, start_Y) -> None:
+        self.tree = Tree(start_X, start_Y)
+        self.problem = Problem(data_path)
+    
+    def _run():
         pass
+
     
