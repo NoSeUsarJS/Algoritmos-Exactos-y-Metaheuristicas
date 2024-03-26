ee9d4fbf229827a5a431490db01e775e81d0a96e
Javier Molina <javier.molina@mail.udp.cl>
2024-03-26 00:47:30 -0300

    Update Node.py

--- Tarea_1/Node.py
+++ Tarea_1/Node.py
@@ -3,14 +3,5 @@ class Node:
     parent = None
     children: list = []
 
-    def __init__(self, X, Y) -> None:
-        self.values = {
-            "X": X,
-            "Y": Y
-        }
-    
-    def _get_child(self, index):
-        return self.children[index]
-
-    def _get_parent(self):
-        return self.parent
+    def __init__(self, X) -> None:
+        self.values = X
